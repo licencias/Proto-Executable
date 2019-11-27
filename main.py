6ccc37527d87d7ef36e649985e7d8929ec142bef
Michael <michael.ferdinand.sson@gmail.com>
2019-11-27 11:40:28 -0300

    v2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_login import login_required
 from flask_mysqldb import MySQL
-
 #INITIALIZATION
 app = Flask(__name__)
 
@@ -14,25 +14,50 @@ mysql = MySQL(app)
 #SETTINGS
 app.secret_key = "mysecretkey"
 
+#ROUTES LOGIN
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if(request.method == 'POST'):
+        if(request.form['usuario'] == "admin" and request.form['clave'] == "admin"):
+            return render_template('enlace.html')
+    return render_template('login.html')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
-@app.route('/form1')
-def form1():
     return render_template('index.html')
 
 @app.route('/enlace')
 def enlace():
     return render_template('enlace.html')
 
-@app.route('/tabla_incidentes')
-def tabla_incidentes():
+@app.route('/tablas')
+def tablas():
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM sg_bd.incidente')
     data = cur.fetchall()
     cur.close()
-    return render_template('tabla_incidentes.html',datos = data)
+    return render_template('tablas.html',datos = data)
+
+@app.route('/tablas2')
+def tablas2():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM sg_bd.incidente LIMIT 3')
+    data = cur.fetchall()
+    cur.close()
+    return render_template('tablas2.html',datos = data)
+
+@app.route('/detalle')
+def detalle():
+    return render_template('detalle.html')
+
+@app.route('/requerimientos')
+def requerimientos():
+    return render_template('requerimientos.html')
+
+
 
 if __name__ == "__main__":
-    app.run(port=8000 ,debug=True)
+    app.run(port=9000 ,debug=True)
+
+
 
